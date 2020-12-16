87fdbd98c58bc6c8e1aeac14caedffcbaf9629af
JoseLG03 <antares069@gmail.com>
2020-12-15 20:18:15 -0600

    conexion a BD

---
+++ funciones.php
@@ -0,0 +1,12 @@
+function conectar()
+{
+    $usuario = "root";
+    $contrasena = "contraseña"; 
+    $servidor = "localhost";
+    $basededatos = "inv_fa";
+
+	$conexion=mysql_connect($servidor, $usuario, "") or die("Problemas en la conexion");
+        mysql_select_db($basededatos);
+}
+
+echo "Hola mundo";
